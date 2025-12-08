ae45d8f1a6e092eaf6312bd937d49edeae71d8f3
Eric Draegerdt <eric.draegerdt@posteo.de>
2025-12-08 13:53:52 +0100

    added POST route to app.py that recieves an id and a qurey and returns a query

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from app.perplexity import talk_to_perplexity
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({"message": "Hello, World!"})
+
+@app.route("/run_query", methods=["POST"])
+def run_query():
+    try:
+        request_data = request.get_json()
+        print("Received request data:", request_data['query'])
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+    return jsonify({'query': request_data['query']})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5004, debug=True)
