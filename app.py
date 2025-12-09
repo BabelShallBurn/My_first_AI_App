b0379bc64bcc78496b7976cab6647b645271862a
Eric Draegerdt <eric.draegerdt@posteo.de>
2025-12-09 11:55:44 +0100

    added validation with pydantic

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
-from flask import Flask, jsonify, request
+from flask import Blueprint, Flask, jsonify, request
+from pydantic import BaseModel, ValidationError
 from app.perplexity import talk_to_perplexity
 
 app = Flask(__name__)
+
+class QueryRequest(BaseModel):
+    id: str
+    query: str
 
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({"message": "Hello, World!"})
 
+
 @app.route("/run_query", methods=["POST"])
 def run_query():
     try:
-        request_data = request.get_json()
+        request_data = QueryRequest(**request.get_json()) .model_dump()
         print("Received request data:", request_data['query'])
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
-    return jsonify({'query': request_data['query']})
+        return jsonify({'query': request_data['query']})
+    except ValidationError as error:
+        return jsonify(error.errors()), 400
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5004, debug=True)
